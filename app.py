b193fee12e574f74fe508c96dedd0af3943c6609
carlosandressoto-lang <carlosandres.soto@confa.com.co>
2026-03-25 01:07:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -246,10 +246,10 @@ if df is not None:
                     pdf.image(img_r2, x=88, y=y_relojes_base+15, w=53)
                     pdf.image(img_r3, x=141, y=y_relojes_base+15, w=53)
                     
-                    pdf.set_font('Helvetica', '', 8); pdf.set_text_color(100, 100, 100)
+                    pdf.set_font('Helvetica', '', 7); pdf.set_text_color(100, 100, 100)
                     niv_manual = ["L7-Visionario", "L6-Mentor", "L5-Autentico", "L4-Facilitador", "L3-Desempeno", "L2-Relaciones", "L1-Crisis"]
                     for i, txt in enumerate(niv_manual):
-                        pdf.text(10, y_relojes_base + 22 + (i * 5.15), txt)
+                        pdf.text(10, y_relojes_base + 15 + (i * 5.15), txt)
                     pdf.set_text_color(0, 0, 0)
 
                     # PÁGINA 2: NINEBOX Y TEXTO IA
